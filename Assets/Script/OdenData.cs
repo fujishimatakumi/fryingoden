cc8965382ca5084e590f58777717d0394237335d
takumi hujishima <fujishima.t425@gmail.com>
2021-03-15 16:27:48 +0900

    おでんのオブジェクトを表現するためのスクリプトを作成ししました

---
+++ Assets/Script/OdenData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OdenData
+{
+    OdenType _type;
+    Sprite _imageIcon;
+
+    public OdenData(OdenType type)
+    {
+        _type = type;
+        _imageIcon = Resources.Load<Sprite>(type.ToString());
+    }
+}
+
+public enum OdenType
+{ 
+    konnyaku,
+    daikon,
+    hanpen
+}
