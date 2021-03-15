2d72d169f922580612b75cb301abd38b7ee1a853
takumi hujishima <fujishima.t425@gmail.com>
2021-03-15 16:05:29 +0900

    ゲームの進行を管理するスクリプトを作成しました

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text _scoreText;
+    [SerializeField] float _timeRimit;
+    [SerializeField] int _maxOder;
+    GameStatus _nowStatus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_nowStatus)
+        {
+            case GameStatus.init:
+                _nowStatus = GameStatus.nowGame;
+                break;
+            case GameStatus.nowGame:
+                DecleaceTime();
+                if (_timeRimit <= 0)
+                {
+                    _nowStatus = GameStatus.gameEnd;
+                }
+                break;
+            case GameStatus.gameEnd:
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void DecleaceTime()
+    {
+        _timeRimit -= Time.deltaTime;
+    }
+}
+public enum GameStatus
+{ 
+    init,
+    nowGame,
+    gameEnd
+}
