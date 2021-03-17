dc73b480ec2469aefc8ae1eb2c924b24f92e514f
takumi hujishima <fujishima.t425@gmail.com>
2021-03-17 12:02:01 +0900

    おでんオブジェクトの移動処理を追加しました

--- Assets/Script/OdenObjectController.cs
+++ Assets/Script/OdenObjectController.cs
@@ -6,20 +6,34 @@ using System;
 [RequireComponent(typeof(SpriteRenderer))]
 public class OdenObjectController : MonoBehaviour
 {
-    OdenData data;
+    [SerializeField] bool _reverceMoveDirecthon;
+    [SerializeField] float _moveSpeed;
+    OdenData _data;
     // Start is called before the first frame update
     void Start()
     {
         var types = Enum.GetValues(typeof(OdenType));
         int index =  UnityEngine.Random.Range(0, (int)OdenType.max);
-        data = new OdenData((OdenType)types.GetValue(index));
-        gameObject.GetComponent<SpriteRenderer>().sprite = data.ImageIcon;
+        _data = new OdenData((OdenType)types.GetValue(index));
+        gameObject.GetComponent<SpriteRenderer>().sprite = _data.ImageIcon;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!_reverceMoveDirecthon)
+        {
+            gameObject.transform.Translate(Vector3.down * _moveSpeed);
+        }
+        else
+        {
+            gameObject.transform.Translate(Vector3.up * _moveSpeed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
