84158dbf8d7b7920340715569cdebaef8275ac88
takumi hujishima <fujishima.t425@gmail.com>
2021-03-17 10:03:56 +0900

    おでんオブジェクトに関するスクリプトを作成しました

---
+++ Assets/Script/OdenObjectController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class OdenObjectController : MonoBehaviour
+{
+    OdenData data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var types = Enum.GetValues(typeof(OdenType));
+        int index =  UnityEngine.Random.Range(0, (int)OdenType.max);
+        data = new OdenData((OdenType)types.GetValue(index));
+        gameObject.GetComponent<SpriteRenderer>().sprite = data.ImageIcon;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CharaController controller;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.gameObject.TryGetComponent(out controller))
+            {
+                //プレイヤーに接触した場合の処理を書く
+            }
+        }
+    }
+}
